32ddedd6d3191ad3c2f259ee0e7550d9d742ac42
hadley <h.wickham@gmail.com>
2014-06-10 14:33:00 -0500

    Improve demo

--- demo/so-9684671.R
+++ demo/so-9684671.R
@@ -1,7 +1,7 @@
 # http://stackoverflow.com/questions/9684671
 set.seed(10)
 activities <- data.frame(
-  id = paste('x', "1.", 1:10, sep = ""),
+  id = sprintf("x1.%02d", 1:10),
   trt = sample(c('cnt', 'tr'), 10, T),
   work.T1 = runif(10),
   play.T1 = runif(10),
@@ -10,7 +10,6 @@ activities <- data.frame(
   play.T2 = runif(10),
   talk.T2 = runif(10)
 )
-
 
 activities %>%
   gather(key, value, -id, -trt) %>%
