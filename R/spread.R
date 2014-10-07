843d910a72d50db1d514ce19b88b0ddf52827121
Matthieu Gomez <gomez.matthieu@gmail.com>
2014-10-06 22:20:28 -0400

    Update spread.R

--- R/spread.R
+++ R/spread.R
@@ -107,6 +107,7 @@ spread_.data.table <- function(data, key_col, value_col, fill = NA, convert = FA
   if (!length_lhs) {
     id <- tempname("temp", data)
     data[, (id) := 1:.N] 
+    on.exit(data[, (id) := NULL])
   }
   else{
     if (anyDuplicated(data, by = c(id))) stop("Duplicate identifiers for rows")
@@ -114,7 +115,6 @@ spread_.data.table <- function(data, key_col, value_col, fill = NA, convert = FA
   formula <- reformulate(termlabels = key_col , response = id)
   data2 <- dcast.data.table(data, formula, value.var = value_col, fill = fill)
   if (!length_lhs) {
-    data[, (id) := NULL]
     data2[, (id) := NULL]
   }
   if (convert) {
