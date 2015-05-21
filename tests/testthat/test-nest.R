28528efecf3d72d1fa3735791eca5d4bd0df09e7
hadley <h.wickham@gmail.com>
2015-05-21 15:55:43 -0500

    Add test for nest

---
+++ tests/testthat/test-nest.R
@@ -0,0 +1,8 @@
+context("nest")
+
+test_that("nest turns grouped values into list-cols", {
+  df <- data_frame(x = c(1, 1, 1), y = 1:3)
+  out <- nest(df, y)
+  expect_equal(out$x, 1)
+  expect_equal(out$y, list(1:3))
+})
