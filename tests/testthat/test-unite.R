7b3725b124d072c6bb5e34124e9d8522a81c8277
hadley <h.wickham@gmail.com>
2015-05-21 15:57:58 -0500

    Add test for unite

---
+++ tests/testthat/test-unite.R
@@ -0,0 +1,9 @@
+context("unite")
+
+test_that("unite pastes columns together & removes old col", {
+  df <- data_frame(x = "a", y = "b")
+  out <- unite(df, z, x:y)
+
+  expect_equal(names(out), "z")
+  expect_equal(out$z, "a_b")
+})
