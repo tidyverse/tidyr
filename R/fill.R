518c9505afc6e181efed28bcd71a9c58a48de7e6
hadley <h.wickham@gmail.com>
2015-05-22 10:24:21 -0500

    Test all variable types in fill.
    Also support filling multiple columns in one step

--- R/fill.R
+++ R/fill.R
@@ -8,16 +8,18 @@ NULL
 #' common output format where values are not repeated, they're recorded
 #' each time they change.
 #'
-#' @param col Bare column name.
+#' @param ... Specification of columns to fill. Use bare variable names.
+#'   Select all variables between x and z with \code{x:z}, exclude y with
+#'   \code{-y}. For more options, see the \link[dplyr]{select} documentation.
 #' @export
 #' @inheritParams extract_
 #' @export
 #' @examples
 #' df <- data.frame(Month = 1:12, Year = c(2000, rep(NA, 11)))
 #' df %>% fill(Year)
-fill <- function(data, col) {
-  col <- col_name(substitute(col))
-  fill_(data, col)
+fill <- function(data, ...) {
+  fill_cols <- unname(dplyr::select_vars(names(data), ...))
+  fill_(data, fill_cols)
 }
 
 #' Standard-evaluation version of \code{fill}.
@@ -25,18 +27,20 @@ fill <- function(data, col) {
 #' This is a S3 generic.
 #'
 #' @param data A data frame.
-#' @param col Name of column to split, as string.
+#' @param fill_cols Character vector of column names.
 #' @keywords internal
 #' @export
-fill_ <- function(data, col) {
+fill_ <- function(data, fill_cols) {
   UseMethod("fill_")
 }
 
 #' @export
-fill_.data.frame <- function(data, col) {
-  old_attr <- attributes(data[[col]])
-  data[[col]] <- fillVector(data[[col]])
-  attributes(data[[col]]) <- old_attr
+fill_.data.frame <- function(data, fill_cols) {
+  for (col in fill_cols) {
+    old_attr <- attributes(data[[col]])
+    data[[col]] <- fillVector(data[[col]])
+    attributes(data[[col]]) <- old_attr
+  }
 
   data
 }
--- tests/testthat/test-fill.R
+++ tests/testthat/test-fill.R
@@ -1,10 +1,15 @@
 context("fill")
 
 test_that("all missings left unchanged", {
-  df <- data_frame(x = c(NA, NA, NA))
+  df <- data_frame(
+    lgl = c(NA, NA),
+    int = c(NA_integer_, NA),
+    dbl = c(NA_real_, NA),
+    chr = c(NA_character_, NA)
+  )
 
-  out <- fill(df, x)
-  expect_equal(out$x, c(NA, NA, NA))
+  out <- fill(df, lgl, int, dbl, chr)
+  expect_equal(out, df)
 })
 
 test_that("missings filled from last non-missing", {
@@ -12,4 +17,19 @@ test_that("missings filled from last non-missing", {
 
   out <- fill(df, x)
   expect_equal(out$x, c(1, 1, 1))
+})
+
+test_that("missings filled for each atomic vector", {
+  df <- data_frame(
+    lgl = c(T, NA),
+    int = c(1L, NA),
+    dbl = c(1, NA),
+    chr = c("a", NA)
+  )
+
+  out <- fill(df, lgl, int, dbl, chr)
+  expect_equal(out$lgl, c(TRUE, TRUE))
+  expect_equal(out$int, c(1L, 1L))
+  expect_equal(out$dbl, c(1, 1))
+  expect_equal(out$chr, c("a", "a"))
 })
