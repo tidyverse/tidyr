249f1b070f4aa46d9212bb98c35a2d45140abc8b
aaronwolen <aaron@wolen.com>
2015-05-26 11:42:00 -0400

    Allow unnest_string to work with multiple columns

--- R/unnest-string.R
+++ R/unnest-string.R
@@ -4,6 +4,9 @@
 #' separates the values and places each one in its own row.
 #'
 #' @inheritParams unnest_string_
+#' @param ... Specification of columns to unnest. Use bare variable names.
+#'   Select all variables between x and z with \code{x:z}, exclude y with
+#'   \code{-y}. For more options, see the \link[dplyr]{select} documentation.
 #' @export
 #' @examples
 #'
@@ -13,9 +16,9 @@
 #'   stringsAsFactors = FALSE
 #' )
 #' unnest_string(df, y)
-unnest_string <- function(data, col, sep = "[^[:alnum:]]+") {
-  col <- col_name(substitute(col))
-  unnest_string_(data, col, sep)
+unnest_string <- function(data, ..., sep = "[^[:alnum:]]+") {
+  unnest_cols <- unname(dplyr::select_vars(names(data), ...))
+  unnest_string_(data, unnest_cols, sep)
 }
 
 #' Standard-evaluation version of \code{unnest_string}.
@@ -23,18 +26,18 @@ unnest_string <- function(data, col, sep = "[^[:alnum:]]+") {
 #' This is a S3 generic.
 #'
 #' @param data A data frame.
-#' @param col Name of column that needs to be unnested.
+#' @param unnest_cols Name of columns that need to be unnested.
 #' @param sep Separator delimiting collapsed values.
 #' @export
-unnest_string_ <- function(data, col, sep = "[^[:alnum:]]+") UseMethod("unnest_string_")
+unnest_string_ <- function(data, unnest_cols, sep = "[^[:alnum:]]+") UseMethod("unnest_string_")
 
 #' @export
-unnest_string_.data.frame <- function(data, col, sep = "[^[:alnum:]]+") {
-  data[[col]] <- stringi::stri_split_regex(data[[col]], sep)
-  unnest_(data, col)
+unnest_string_.data.frame <- function(data, unnest_cols, sep = "[^[:alnum:]]+") {
+  data[unnest_cols] <- lapply(data[unnest_cols], stringi::stri_split_regex, sep)
+  unnest_(data, unnest_cols)
 }
 
 #' @export
-unnest_string_.tbl_df <- function(data, col, sep = "[^[:alnum:]]+") {
+unnest_string_.tbl_df <- function(data, unnest_cols, sep = "[^[:alnum:]]+") {
   dplyr::tbl_df(NextMethod())
 }
