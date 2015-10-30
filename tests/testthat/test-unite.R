cece0b911e7c6c46c2150bf700d56f74f72be812
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-10-30 23:59:11 +0100

    change order in unite
    first remove old columns, then add new

--- tests/testthat/test-unite.R
+++ tests/testthat/test-unite.R
@@ -7,3 +7,11 @@ test_that("unite pastes columns together & removes old col", {
   expect_equal(names(out), "z")
   expect_equal(out$z, "a_b")
 })
+
+test_that("unite does not remove new col in case of name clash", {
+  df <- dplyr::data_frame(x = "a", y = "b")
+  out <- unite(df, x, x:y)
+
+  expect_equal(names(out), "x")
+  expect_equal(out$x, "a_b")
+})
