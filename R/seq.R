80577962085b9702d420575d7d5fecd7a2ca5fce
hadley <h.wickham@gmail.com>
2015-12-31 10:24:48 -0600

    First pass at seq_time

--- R/seq.R
+++ R/seq.R
@@ -9,3 +9,13 @@ seq_range <- function(x, n) {
   rng <- range(x, na.rm = TRUE)
   seq(rng[1], rng[2], length = n)
 }
+
+
+seq_time <- function(x, period) {
+  if (any(x %% period > 1e-6)) {
+    stop("`x` is not a regular sequence.", call. = FALSE)
+  }
+
+  rng <- range(x, na.rm = TRUE)
+  seq(rng[1], rng[2], by = period)
+}
