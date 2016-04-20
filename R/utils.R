c889f4a0eca0eee17a566db3bcdb42987e9f3c88
hadley <h.wickham@gmail.com>
2016-04-20 10:30:21 -0500

    Hack around dplyr 0.4/0.5 incompatibility
    cc @daattali - this might work for your problem too.

--- R/utils.R
+++ R/utils.R
@@ -69,3 +69,7 @@ regroup <- function(x, y, except) {
 
   dplyr::grouped_df(y, group_vars)
 }
+
+# Allows tests to work with either dplyr 0.4 (which ignores value of
+# everything), and 0.5 which exports it as a proper function
+everything <- function(...) dplyr::everything(...)
