28af2f1c0f5d518f605b56b79e3c64f1ee2cd00d
Lionel Henry <lionel.hry@gmail.com>
2018-11-22 11:51:16 +0100

    Don't import questioning or deprecated purrr functions

--- R/tidyr.R
+++ R/tidyr.R
@@ -1,11 +1,10 @@
 #' @keywords internal
 #' @import rlang
 #' @importFrom glue glue
-#' @importFrom purrr accumulate accumulate_right discard every keep
-#'   map map2 map2_chr map2_dbl map2_df map2_int map2_lgl map_at
-#'   map_call map_chr map_dbl map_df map_if map_int map_lgl pmap
-#'   pmap_chr pmap_dbl pmap_df pmap_int pmap_lgl reduce reduce_right
-#'   some transpose
+#' @importFrom purrr accumulate discard every keep map map2 map2_chr
+#'   map2_dbl map2_df map2_int map2_lgl map_at map_chr map_dbl map_df
+#'   map_if map_int map_lgl pmap pmap_chr pmap_dbl pmap_df pmap_int
+#'   pmap_lgl reduce some transpose
 #' @importFrom tibble tibble as_tibble
 #' @importFrom utils type.convert
 #' @useDynLib tidyr, .registration = TRUE
