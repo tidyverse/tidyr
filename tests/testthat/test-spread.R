ba1660df4c18402b3a1ad8a4b7d6ef0eb8e913cf
Julia Silge <julia.silge@gmail.com>
2019-01-19 11:48:53 -0600

    More informative error message for duplicate identifiers for rows. Addresses #426.

--- tests/testthat/test-spread.R
+++ tests/testthat/test-spread.R
@@ -24,7 +24,10 @@ test_that("convert turns strings into integers", {
 test_that("duplicate values for one key is an error", {
   df <- tibble(x = factor(c("a", "b", "b")), y = c(1, 2, 2), z = c(1, 2, 2))
   expect_error(spread(df, x, y),
-    "Duplicate identifiers for rows (2, 3)",
+    paste0("Each row of output must be identified by a unique combination of keys.\n",
+           "Keys are shared for 2 rows:\n",
+           "* 2, 3\n",
+           "Do you need to create unique ID with tibble::rowid_to_column()?"),
     fixed = TRUE
   )
 })
