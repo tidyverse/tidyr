b7bfe09fe13f6be80c390222682a6a3b0c85e459
Stu Field <sfield@somalogic.com>
2019-01-19 10:58:34 -0600

    Update to make unconditional tibble conversion

--- R/nest.R
+++ R/nest.R
@@ -38,9 +38,7 @@ nest <- function(data, ..., .key = "data") {
 nest.data.frame <- function(data, ..., .key = "data") {
   key_var <- as_string(ensym2(.key))
 
-  if (!inherits(data, "tbl_df")) {
-    data <- tibble::as_tibble(data)
-  }
+  data <- tibble::as_tibble(data)
 
   nest_vars <- unname(tidyselect::vars_select(names(data), ...))
   if (is_empty(nest_vars)) {
