3531371a2e6c4554342b8a99f93ab24f23233c15
Stu Field <sfield@somalogic.com>
2019-01-19 16:09:11 -0600

    Remove redundant code in nest.data.frame(); simply dispatch nest.tbl_df following tibble coersion.

--- R/nest.R
+++ R/nest.R
@@ -69,34 +69,7 @@ nest.tbl_df <- function(data, ..., .key = "data") {
 
 #' @export
 nest.data.frame <- function(data, ..., .key = "data") {
-
-  key_var   <- as_string(ensym2(.key))
-  data      <- tibble::as_tibble(data)
-  nest_vars <- unname(tidyselect::vars_select(names(data), ...))
-
-  if (is_empty(nest_vars)) {
-    nest_vars <- names(data)
-  }
-
-  if (dplyr::is_grouped_df(data)) {
-    group_vars <- dplyr::group_vars(data)
-  } else {
-    group_vars <- setdiff(names(data), nest_vars)
-  }
-  nest_vars <- setdiff(nest_vars, group_vars)
-
-  data <- dplyr::ungroup(data)
-  if (is_empty(group_vars)) {
-    return(tibble(!! key_var := list(data)))
-  }
-
-  out <- dplyr::select(data, !!! syms(group_vars))
-
-  idx <- dplyr::group_indices(data, !!! syms(group_vars))
-  representatives <- which(!duplicated(idx))
-
-  out <- dplyr::slice(out, representatives)
-  out[[key_var]] <- unname(split(data[nest_vars], idx))[unique(idx)]
-
-  out
+  .key <- enquo(.key)
+  data <- tibble::as_tibble(data)
+  nest.tbl_df(data, ..., .key = !! .key)
 }
