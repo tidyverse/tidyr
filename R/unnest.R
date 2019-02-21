d028342c2d46602ec27d8153d2451e28292747c0
Romain Francois <romain@purrple.cat>
2019-02-21 14:15:21 +0100

    fix list_col_type()
    taking into account the ptype attribute that might have been left by group_nest() in the edge case of a 0 rows nested data frame.

--- R/unnest.R
+++ R/unnest.R
@@ -161,13 +161,13 @@ unnest.data.frame <- function(data, ..., .drop = NA, .id = NULL,
 }
 
 list_col_type <- function(x) {
-  is_data_frame <- map_lgl(x, is.data.frame)
+  is_data_frame <- map_lgl(x, function(x) is.data.frame(x) || is.data.frame(attr(x, "ptype", exact = TRUE)))
   is_atomic <- map_lgl(x, function(x) is_atomic(x) || (is_list(x) && !is.object(x)))
 
-  if (all(is_atomic)) {
-    "atomic"
-  } else if (all(is_data_frame)) {
+  if (all(is_data_frame)) {
     "dataframe"
+  } else if (all(is_atomic)) {
+    "atomic"
   } else {
     "mixed"
   }
