2f664e489b01ac24c29ca98a07d842dc822775b1
T D James <tdjames1@users.noreply.github.com>
2019-05-07 12:37:10 +0100

    Fix typo in extract() documentation

--- R/extract.R
+++ R/extract.R
@@ -13,7 +13,7 @@
 #'   names or column positions).
 #' @param into Names of new variables to create as character vector.
 #' @param regex a regular expression used to extract the desired values.
-#'   The should be one group (defined by `()`) for each element of `into`.
+#'   There should be one group (defined by `()`) for each element of `into`.
 #' @param remove If `TRUE`, remove input column from output data frame.
 #' @param convert If `TRUE`, will run [type.convert()] with
 #'   `as.is = TRUE` on new columns. This is useful if the component
