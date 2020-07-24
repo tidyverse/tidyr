514d9692a2b93f5fcabb833594f83c65c5ead541
Hiroaki Yutani <yutani.ini@gmail.com>
2020-07-24 23:47:04 +0900

    Revert "Stop re-exporting tidyselect helpers"
    This reverts commit 4756bb3e66881a81f4cf91460589e13f3b6c2b49.

--- R/tidyr.R
+++ R/tidyr.R
@@ -27,8 +27,29 @@ tibble::tibble
 #' @export
 tibble::as_tibble
 
-# TODO: This is left just to minimize the breakage on a minor release, Remove
-# this on a bigger release. (c.f. https://github.com/tidyverse/tidyr/pull/1009#discussion_r460013419)
+
+#' @aliases select_helpers
+#' @importFrom tidyselect contains
+#' @export
+tidyselect::contains
+#' @importFrom tidyselect ends_with
+#' @export
+tidyselect::ends_with
+#' @importFrom tidyselect everything
+#' @export
+tidyselect::everything
+#' @importFrom tidyselect matches
+#' @export
+tidyselect::matches
+#' @importFrom tidyselect num_range
+#' @export
+tidyselect::num_range
+#' @importFrom tidyselect one_of
+#' @export
+tidyselect::one_of
 #' @importFrom tidyselect starts_with
 #' @export
 tidyselect::starts_with
+#' @importFrom tidyselect last_col
+#' @export
+tidyselect::last_col
