5c39e3bfab827e3290ea4495d70d3de118fb0abb
Maximilian Girlich <maximilian.girlich@metoda.com>
2020-12-08 09:22:40 +0100

    add test for data.table and empty key_vars

--- tests/testthat/test-pivot-wide.R
+++ tests/testthat/test-pivot-wide.R
@@ -65,6 +65,16 @@ test_that("data frame columns pivot correctly", {
   expect_equal(out$b$y, 7:8)
 })
 
+test_that("works with data.table and empty key_vars", {
+  skip_if_not_installed("data.table")
+
+  dt <- data.table::data.table(n = "a", v = 1)
+  expect_equal(
+    pivot_wider(dt, names_from = n, values_from = v),
+    tibble(a = 1)
+  )
+})
+
 
 # column names -------------------------------------------------------------
 
