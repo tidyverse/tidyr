a0337721c5f5376d487823f484fb1633e170ad0f
Hadley Wickham <h.wickham@gmail.com>
2021-02-18 08:15:20 -0600

    Add comment

--- R/rectangle.R
+++ R/rectangle.R
@@ -126,6 +126,9 @@ hoist <- function(.data, .col, ..., .remove = TRUE, .simplify = TRUE, .ptype = l
 
   pluckers <- check_pluckers(...)
 
+  # In R <4.1, `::` is quite slow and this is a tight loop, so eliminating
+  # the lookup has a large performance impact:
+  # https://github.com/tidyverse/tidyr/issues/1001
   pluck <- purrr::pluck
   new_cols <- map(pluckers, function(idx) {
     map(x, ~ pluck(.x, !!!idx))
