5ac1b5428d0c8594007a7d5d9a07cde4d16d01c7
DavisVaughan <davis@rstudio.com>
2021-03-19 16:03:59 -0400

    Add extra tests for `replace_na()`

---
+++ tests/testthat/_snaps/replace_na.md
@@ -0,0 +1,10 @@
+# replacement must be castable to `data`
+
+    Can't convert from `replace` <double> to `data` <integer> due to loss of precision.
+    * Locations: 1
+
+# replacement must be castable to corresponding column
+
+    Can't convert from `value$a` <double> to `data$a` <integer> due to loss of precision.
+    * Locations: 1
+
--- tests/testthat/test-replace_na.R
+++ tests/testthat/test-replace_na.R
@@ -14,6 +14,19 @@ test_that("can only be length 0", {
   expect_error(replace_na(1, 1:10), "length 10, not length 1")
 })
 
+test_that("can replace missing rows in arrays", {
+  x <- matrix(c(NA, NA, NA, 6), nrow = 2)
+  replace <- matrix(c(-1, -2), nrow = 1)
+  expect <- matrix(c(-1, NA, -2, 6), nrow = 2)
+
+  expect_identical(replace_na(x, replace), expect)
+})
+
+test_that("replacement must be castable to `data`", {
+  x <- c(1L, NA)
+  expect_snapshot_error(replace_na(x, 1.5))
+})
+
 # data frame -------------------------------------------------------------
 
 test_that("empty call does nothing", {
@@ -39,4 +52,24 @@ test_that("can replace NULLs in list-column", {
   rs <- replace_na(df, list(x = list(1:5)))
 
   expect_identical(rs, tibble(x = list(1, 1:5)))
+})
+
+test_that("df-col rows must be completely missing to be replaceable", {
+  col <- tibble(x = c(1, NA, NA), y = c(1, 2, NA))
+  df <- tibble(a = col)
+
+  col <- tibble(x = c(1, NA, -1), y = c(1, 2, -2))
+  expect <- tibble(a = col)
+
+  replace <- tibble(x = -1, y = -2)
+
+  expect_identical(
+    replace_na(df, list(a = replace)),
+    expect
+  )
+})
+
+test_that("replacement must be castable to corresponding column", {
+  df <- tibble(a = c(1L, NA))
+  expect_snapshot_error(replace_na(df, list(a = 1.5)))
 })
