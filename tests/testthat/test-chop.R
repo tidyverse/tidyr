8967fb2761ec7fcf602a6c530586c60912553865
DavisVaughan <davis@rstudio.com>
2021-10-27 15:23:48 -0400

    Revert "Style"
    This reverts commit 621f9398ed91956394e2c7106a3ee8fbd55a4ec1.

--- tests/testthat/test-chop.R
+++ tests/testthat/test-chop.R
@@ -16,9 +16,7 @@ test_that("chopping no columns returns input", {
 
 test_that("grouping is preserved", {
   df <- tibble(g = c(1, 1), x = 1:2)
-  out <- df %>%
-    dplyr::group_by(g) %>%
-    chop(x)
+  out <- df %>% dplyr::group_by(g) %>% chop(x)
   expect_equal(dplyr::group_vars(out), "g")
 })
 
@@ -84,9 +82,7 @@ test_that("respects list_of types", {
 
 test_that("grouping is preserved", {
   df <- tibble(g = 1, x = list(1, 2))
-  out <- df %>%
-    dplyr::group_by(g) %>%
-    unchop(x)
+  out <- df %>% dplyr::group_by(g) %>% unchop(x)
   expect_equal(dplyr::group_vars(out), "g")
 })
 
