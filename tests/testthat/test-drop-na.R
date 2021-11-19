d0344e04c543c60b84c0e9a7c9456acc502a99cc
Davis Vaughan <davis@rstudio.com>
2021-11-19 15:04:53 -0500

    Implement `drop_na()` using `vec_detect_complete()` (#1229)
    * Revise `drop_na()` to use `vec_detect_complete()`
    
    * Revise current `drop_na()` tests
    
    * Add new `drop_na()` tests
    
    * Bump required vctrs version
    
    * NEWS bullet
    
    * Add tests for df-cols and rcrd cols

--- tests/testthat/test-drop-na.R
+++ tests/testthat/test-drop-na.R
@@ -1,18 +1,25 @@
 test_that("empty call drops every row", {
   df <- tibble(x = c(1, 2, NA), y = c("a", NA, "b"))
-  exp <- tibble(x = c(1), y = c("a"))
-  res <- tidyr::drop_na(df)
-  expect_equal(res, exp)
+  exp <- tibble(x = 1, y = "a")
+  res <- drop_na(df)
+  expect_identical(res, exp)
+})
+
+test_that("tidyselection that selects no columns doesn't drop any rows (#1227)", {
+  df <- tibble(x = c(1, 2, NA), y = c("a", NA, "b"))
+  expect_identical(drop_na(df, starts_with("foo")), df)
 })
 
 test_that("specifying (a) variables considers only that variable(s)", {
   df <- tibble(x = c(1, 2, NA), y = c("a", NA, "b"))
+
   exp <- tibble(x = c(1, 2), y = c("a", NA))
-  res <- tidyr::drop_na(df, x)
-  expect_equal(res, exp)
+  res <- drop_na(df, x)
+  expect_identical(res, exp)
+
   exp <- tibble(x = c(1), y = c("a"))
-  res <- tidyr::drop_na(df, x:y)
-  expect_equal(res, exp)
+  res <- drop_na(df, x:y)
+  expect_identical(res, exp)
 })
 
 test_that("groups are preserved", {
@@ -22,28 +29,23 @@ test_that("groups are preserved", {
   gdf <- dplyr::group_by(df, "g")
   gexp <- dplyr::group_by(exp, "g")
 
-  res <- tidyr::drop_na(gdf, y)
-  expect_equal(res, gexp)
-  expect_equal(dplyr::group_vars(res), dplyr::group_vars(gexp))
-})
+  res <- drop_na(gdf, y)
 
-test_that("empty call drops every row", {
-  df <- tibble(x = c(1, 2, NA), y = c("a", NA, "b"))
-  res <- tidyr::drop_na(df)
-  expect_identical(res, tibble(x = 1, y = "a"))
+  expect_identical(res, gexp)
+  expect_identical(dplyr::group_vars(res), dplyr::group_vars(gexp))
 })
 
 test_that("errors are raised", {
   df <- tibble(x = c(1, 2, NA), y = c("a", NA, "b"))
-  expect_error(tidyr::drop_na(df, !! list()))
-  expect_error(tidyr::drop_na(df, "z"))
+  expect_snapshot((expect_error(drop_na(df, list()))))
+  expect_snapshot((expect_error(drop_na(df, "z"))))
 })
 
 test_that("single variable data.frame doesn't lose dimension", {
   df <- data.frame(x = c(1, 2, NA))
-  res <- tidyr::drop_na(df, "x")
+  res <- drop_na(df, "x")
   exp <- data.frame(x = c(1, 2))
-  expect_equal(res, exp)
+  expect_identical(res, exp)
 })
 
 test_that("works with list-cols", {
@@ -53,9 +55,36 @@ test_that("works with list-cols", {
   expect_identical(rs, tibble(x = list(1L), y = 1L))
 })
 
+test_that("doesn't drop empty atomic elements of list-cols (#1228)", {
+  df <- tibble(x = list(1L, NULL, integer()))
+  expect_identical(drop_na(df), df[c(1, 3),])
+})
+
 test_that("preserves attributes", {
   df <- tibble(x = structure(c(1, NA), attr = "!"))
   rs <- drop_na(df)
 
-  expect_equal(rs$x, structure(1, attr = "!"))
+  expect_identical(rs$x, structure(1, attr = "!"))
+})
+
+test_that("works with df-cols", {
+  # if any packed row contains a missing value, it is incomplete
+  df <- tibble(a = tibble(x = c(1, 1, NA, NA), y = c(1, NA, 1, NA)))
+  expect_identical(drop_na(df, a), tibble(a = tibble(x = 1, y = 1)))
+})
+
+test_that("works with rcrd cols", {
+  skip_if(
+    packageVersion("vctrs") <= "0.3.8",
+    "vec_detect_complete() treated rcrds differently"
+  )
+
+  # if any rcrd field contains a missing value, it is incomplete
+  col <- new_rcrd(list(x = c(1, 1, NA, NA), y = c(1, NA, 1, NA)))
+  df <- tibble(col = col)
+
+  expect_identical(
+    drop_na(df, col),
+    tibble(col = new_rcrd(list(x = 1, y = 1)))
+  )
 })
