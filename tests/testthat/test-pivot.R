fb6a3777648bdf9efcc9b2c68a004268fdb197c9
Davis Vaughan <davis@rstudio.com>
2021-11-23 16:38:31 -0500

    Reorder values columns before combining with id columns (#1236)
    * Add additional checks to spec columns
    
    In particular, `.name` must be unique. In the case of a `pivot_wider()`, we need to be able to manipulate the column order of the widened columns using the original order supplied in `.name`. This means we can't have name repair altering the names midway through the pivot. For `pivot_longer()`, each `.name` must uniquely identify the row in the spec.
    
    * Reorder values columns before combining with id columns
    
    Since `.name` was checked to be unique, the values columns shouldn't have any name repair applied to them and should match `.name` exactly. We reorder those to match what the user supplied, then combine with the id columns, at which point we apply name repair right before returning.
    
    * NEWS bullet

--- tests/testthat/test-pivot.R
+++ tests/testthat/test-pivot.R
@@ -2,3 +2,18 @@ test_that("basic sanity checks for spec occur", {
   expect_error(check_spec(1), "data.frame")
   expect_error(check_spec(mtcars), ".name")
 })
+
+test_that("`.name` column must be a character vector", {
+  df <- tibble(.name = 1:2, .value = c("a", "b"))
+  expect_snapshot((expect_error(check_spec(df))))
+})
+
+test_that("`.value` column must be a character vector", {
+  df <- tibble(.name = c("x", "y"), .value = 1:2)
+  expect_snapshot((expect_error(check_spec(df))))
+})
+
+test_that("`.name` column must be unique", {
+  df <- tibble(.name = c("x", "x"), .value = c("a", "b"))
+  expect_snapshot((expect_error(check_spec(df))))
+})
