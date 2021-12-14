5e967fb2ad0bdc3b9331a09e698c879485ba5637
DavisVaughan <davis@rstudio.com>
2021-12-14 11:46:57 -0500

    Document and export `check_pivot_spec()`

--- R/pivot.R
+++ R/pivot.R
@@ -1,3 +1,34 @@
+#' Check assumptions about a pivot `spec`
+#'
+#' @description
+#' `check_pivot_spec()` is a developer facing helper function for validating
+#' the pivot spec used in [pivot_longer_spec()] or [pivot_wider_spec()]. It is
+#' only useful if you are extending [pivot_longer()] or [pivot_wider()] with
+#' new S3 methods.
+#'
+#' `check_pivot_spec()` makes the following assertions:
+#' - `spec` must be a data frame.
+#' - `spec` must have a character column named `.name`.
+#' - `spec` must have a character column named `.value`.
+#' - The `.name` column must be unique.
+#' - The `.name` and `.value` columns must be the first two columns in the data
+#'   frame, and will be reordered if that is not true.
+#'
+#' @inheritParams pivot_wider_spec
+#'
+#' @keywords internal
+#' @export
+#' @examples
+#' # A valid spec
+#' spec <- tibble(.name = "a", .value = "b", foo = 1)
+#' check_pivot_spec(spec)
+#'
+#' spec <- tibble(.name = "a")
+#' try(check_pivot_spec(spec))
+#'
+#' # `.name` and `.value` are forced to be the first two columns
+#' spec <- tibble(foo = 1, .value = "b", .name = "a")
+#' check_pivot_spec(spec)
 check_pivot_spec <- function(spec) {
   if (!is.data.frame(spec)) {
     abort("`spec` must be a data frame.")
