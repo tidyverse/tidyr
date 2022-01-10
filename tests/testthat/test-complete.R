2ea67e7fb2c856f63b1e9b12abf2e41c099c759a
DavisVaughan <davis@rstudio.com>
2022-01-10 17:55:03 -0500

    Only allow access to the non-group variables in `complete()`

--- tests/testthat/test-complete.R
+++ tests/testthat/test-complete.R
@@ -9,33 +9,87 @@ test_that("basic invocation works", {
   expect_equal(out$z, c(3, NA, NA, 4))
 })
 
-test_that("preserves grouping", {
-  local_options(lifecycle_verbosity = "quiet")
+test_that("will complete within each group (#396)", {
+  levels <- c("a", "b", "c")
 
-  df <- tibble(x = 1:2, y = 1:2, z = 3:4) %>% dplyr::group_by(x)
-  out <- complete(df, x, y)
-  expect_s3_class(out, "grouped_df")
-  expect_equal(dplyr::group_vars(out), dplyr::group_vars(df))
-})
-
-test_that("complete allows expanding on grouping variable (#396)", {
   df <- tibble(
-    g = factor(c("a", "b", "b")),
-    a = c(1, 1, 3),
-    x = c(1, 2, 3)
+    g = c("a", "b", "a"),
+    a = c(1L, 1L, 2L),
+    b = factor(c("a", "a", "b"), levels = levels),
+    c = c(4, 5, 6)
   )
   gdf <- dplyr::group_by(df, g)
 
-  out <- complete(gdf, g, a)
+  out <- complete(gdf, a, b)
 
-  # Same as split by group, complete, combine
-  expect <- vec_rbind(
-    complete(df[1,], g, a),
-    complete(df[2:3,], g, a)
+  # Still grouped
+  expect_identical(dplyr::group_vars(out), "g")
+
+  out <- nest(out, data = -g)
+
+  expect_identical(
+    out$data[[1]],
+    tibble(
+      a = vec_rep_each(c(1L, 2L), times = 3),
+      b = factor(vec_rep(c("a", "b", "c"), times = 2)),
+      c = c(4, NA, NA, NA, 6, NA)
+    )
   )
-  expect <- dplyr::group_by(expect, g)
 
-  expect_identical(out, expect)
+  expect_identical(
+    out$data[[2]],
+    tibble(
+      a = 1L,
+      b = factor(c("a", "b", "c")),
+      c = c(5, NA, NA)
+    )
+  )
+})
+
+test_that("complete does not allow expansion on grouping variable (#1299)", {
+  df <- tibble(
+    g = "x",
+    a = 1L
+  )
+  gdf <- dplyr::group_by(df, g)
+
+  # This is a dplyr error that we don't own
+  expect_error(complete(gdf, g))
+})
+
+test_that("can use `.drop = FALSE` with complete (#1299)", {
+  levels <- c("a", "b", "c")
+
+  df <- tibble(
+    g = factor(c("a", "b", "a"), levels = levels),
+    a = c(1L, 1L, 2L),
+    b = factor(c("a", "a", "b"), levels = levels)
+  )
+  gdf <- dplyr::group_by(df, g, .drop = FALSE)
+
+  # No data in group "c" for `a`, so we don't get that in the result
+  expect_identical(
+    complete(gdf, a),
+    vec_sort(gdf)
+  )
+
+  expect <- crossing(g = factor(levels = levels), b = factor(levels = levels))
+  expect <- dplyr::group_by(expect, g, .drop = FALSE)
+  expect <- dplyr::full_join(expect, df, c("g", "b"))
+
+  # Levels of empty vector in `b` are expanded for group "c"
+  expect_identical(complete(gdf, b), expect)
+})
+
+test_that("complete moves the grouping and completing variables to the front", {
+  df <- tibble(
+    a = 1L,
+    g = "x",
+    b = 2L
+  )
+  gdf <- dplyr::group_by(df, g)
+
+  expect_named(complete(gdf, b), c("g", "b", "a"))
 })
 
 test_that("expands empty factors", {
@@ -104,6 +158,19 @@ test_that("can limit the fill to only implicit missing values with `explicit` (#
     complete(df, x, fill = list(a = 0), explicit = FALSE),
     tibble(x = factor(1:3), a = c(1, NA, 0))
   )
+})
+
+test_that("can't fill a grouping column", {
+  df <- tibble(
+    g = c(1, NA),
+    x = factor(1:2, levels = 1:3)
+  )
+  gdf <- dplyr::group_by(df, g)
+
+  # Silently ignore it
+  out <- complete(gdf, x, fill = list(g = 0))
+
+  expect_identical(out$g, c(1, 1, 1, NA, NA, NA))
 })
 
 test_that("if the completing variables have missings, `fill` will fill them after expansion", {
