d03dd8dc0e50835a41e09487009bdcdd120992e5
Hadley Wickham <h.wickham@gmail.com>
2022-03-09 14:00:47 -0600

    Add CMS datasets to demonstrate pivot_wider()

--- R/data.R
+++ R/data.R
@@ -200,3 +200,67 @@
 #'   \item{name_child2}{Name of second child}
 #' }
 "family"
+
+#' Data from the Centers for Medicare & Medicaid Services
+#'
+#' @description
+#' Two datasets from public data provided the Centers for Medicare & Medicaid
+#' Services, <https://data.cms.gov>.
+#'
+#' *  `cms_patient_experience` contains some lightly cleaned data from
+#'    "Hospice - Provider Data", which provides a list of hospice agencies
+#'    along with some data on quality of patient care,
+#'    <https://data.cms.gov/provider-data/dataset/252m-zfp9>.
+#'
+#' * `cms_patient_care` "Doctors and Clinicians Quality Payment Program PY 2020
+#'   Virtual Group Public Reporting",
+#'   <https://data.cms.gov/provider-data/dataset/8c70-d353>
+#'
+#' @examples
+#' cms_patient_experience %>%
+#'   dplyr::distinct(measure_cd, measure_title)
+#'
+#' cms_patient_experience %>%
+#'   pivot_wider(
+#'     id_cols = starts_with("org"),
+#'     names_from = measure_cd,
+#'     values_from = prf_rate
+#'  )
+#'
+#' cms_patient_care %>%
+#'   pivot_wider(
+#'     names_from = type,
+#'     values_from = score
+#'   )
+#'
+#' cms_patient_care %>%
+#'   pivot_wider(
+#'     names_from = measure_abbr,
+#'     values_from = score
+#'   )
+#'
+#' cms_patient_care %>%
+#'   pivot_wider(
+#'     names_from = c(measure_abbr, type),
+#'     values_from = score
+#'   )
+#' @format `cms_patient_experience` is a data frame with 500 observations and
+#'    five variables:
+#' \describe{
+#' \item{org_pac_id,org_nm}{Organisation ID and name}
+#' \item{measure_cd,measure_title}{Measure code and title}
+#' \item{prf_rate}{Measure performance rate}
+#' }
+"cms_patient_experience"
+
+#' @format `cms_patient_care` is a data frame with 252 observations and
+#'    five variables:
+#' \describe{
+#' \item{ccn,facility_name}{Facility ID and name}
+#' \item{measure_abbr}{Abbreviated measurement title, suitable for use as variable name}
+#' \item{score}{Measure score}
+#' \item{type}{Whether score refers to the rating out of 100 ("observed"), or
+#'   the maximum possible value of the raw score ("denominator")}
+#' }
+#' @rdname cms_patient_experience
+"cms_patient_care"
---
+++ data-raw/cms.R
@@ -0,0 +1,61 @@
+library(tidyverse)
+
+# Doctors and Clinicians Quality Payment Program PY 2020 Group Public Reporting:
+# Patient Experience
+
+# https://data.cms.gov/provider-data/dataset/8c70-d353
+url <- "https://data.cms.gov/provider-data/api/1/datastore/query/8c70-d353/0?offset=0&count=true&results=true&schema=true&keys=true&format=json&rowIds=false"
+
+json <- jsonlite::read_json(url)
+cms_patient_experience <- json$results |>
+  map_df(as_tibble) |>
+  select(org_pac_id, org_nm, measure_cd, measure_title, prf_rate) |>
+  arrange(org_pac_id, stringi::stri_rank(measure_cd, list(numeric = TRUE))) |>
+  mutate(prf_rate = as.numeric(prf_rate))
+
+write_csv(cms_patient_experience, "data-raw/cms_patient_experience.csv")
+usethis::use_data(cms_patient_experience, overwrite = TRUE)
+
+
+# -------------------------------------------------------------------------
+
+# Hospice - Provider Data
+# A list of hospice agencies with data on the quality of patient care measures.
+# https://data.cms.gov/provider-data/dataset/252m-zfp9
+
+# Recommended by
+# https://twitter.com/hunter_boost/status/1500212341463339008
+
+url <- "https://data.cms.gov/provider-data/api/1/datastore/query/252m-zfp9/0?limit=500&offset=0&count=true&results=true&schema=true&keys=true&format=json&rowIds=false"
+json <- jsonlite::read_json(url)
+
+abbr <- tribble(
+  ~measure_name                                                 , ~measure_abbr,
+  "Hospice and Palliative Care Treatment Preferences"           , "treat_pref",
+  "Beliefs & Values Addressed (if desired by the patient)"      , "beliefs_addressed",
+  "Hospice and Palliative Care Pain Screening"                  , "pain_screening",
+  "Hospice and Palliative Care Pain Assessment"                 , "pain_assessment",
+  "Hospice and Palliative Care Dyspnea Screening"               , "dyspnea_screening",
+  "Hospice and Palliative Care Dyspnea Treatment"               , "dyspena_treatment",
+  "Patient Treated with an Opioid Who Are Given a Bowel Regimen", "opioid_bowel",
+  "Hospice and Palliative Care Composite Process Measure"       , "composite_process",
+  "Hospice Visits When Death Is Imminent, Measure 1"            , "visits_imminent",
+)
+
+cms_patient_care <- json$results |>
+  map_df(as_tibble) |>
+  select(ccn = cms_certification_number_ccn, facility_name, measure_name, measure_code, score) |>
+  mutate(measure_name = na_if(measure_name, "")) |>
+  fill(measure_name, .direction = "up") |>
+  filter(str_detect(measure_code, "^H")) |>
+  mutate(score = as.numeric(na_if(score, "Not Available"))) |>
+  mutate(
+    type = str_to_lower(str_remove(measure_code, "H_\\d{3}_\\d{2}_")),
+    measure_code = NULL
+  ) |>
+  left_join(abbr, by = "measure_name") |>
+  select(ccn, facility_name, measure_abbr, score, type) |>
+  arrange(ccn, measure_abbr, type)
+
+write_csv(cms_patient_care, "data-raw/cms_patient_care.csv")
+usethis::use_data(cms_patient_care, overwrite = TRUE)
