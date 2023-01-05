3567eb768f46b873ab95272f3755809e9123f166
DavisVaughan <davis@rstudio.com>
2023-01-05 09:31:58 -0500

    Update tests to use `df_append()` rather than `append_df()`

--- tests/testthat/_snaps/append.md
+++ tests/testthat/_snaps/append.md
@@ -1,11 +1,11 @@
 # after must be integer or character
 
     Code
-      (expect_error(append_df(df1, df2, after = 1)))
+      (expect_error(df_append(df1, df2, after = 1.5)))
     Output
       <error/rlang_error>
-      Error in `append_df()`:
-      ! `after` must be character or integer
+      Error in `df_append()`:
+      ! `after` must be a whole number, not the number 1.5.
       i This is an internal error that was detected in the tidyr package.
         Please report it at <https://github.com/tidyverse/tidyr/issues> with a reprex (<https://https://tidyverse.org/help/>) and the full backtrace.
 
--- tests/testthat/test-append.R
+++ tests/testthat/test-append.R
@@ -2,39 +2,67 @@ test_that("columns in y replace those in x", {
   df1 <- data.frame(x = 1)
   df2 <- data.frame(x = 2)
 
-  expect_equal(append_df(df1, df2), df2)
+  expect_equal(df_append(df1, df2), df2)
+})
+
+test_that("replaced columns retain the correct ordering (#1444)", {
+  df1 <- data.frame(
+    x = 1,
+    y = 2,
+    z = 3
+  )
+  df2 <- data.frame(x = 4)
+
+  expect_identical(
+    df_append(df1, df2, after = 0L),
+    data.frame(x = 4, y = 2, z = 3)
+  )
+  expect_identical(
+    df_append(df1, df2, after = 1L),
+    data.frame(x = 4, y = 2, z = 3)
+  )
+  expect_identical(
+    df_append(df1, df2, after = 2L),
+    data.frame(y = 2, x = 4, z = 3)
+  )
 })
 
 test_that("after must be integer or character", {
   df1 <- data.frame(x = 1)
   df2 <- data.frame(x = 2)
 
-  expect_snapshot((expect_error(append_df(df1, df2, after = 1))))
+  expect_snapshot((expect_error(df_append(df1, df2, after = 1.5))))
 })
 
+test_that("always returns a bare data frame", {
+  df1 <- tibble(x = 1)
+  df2 <- tibble(y = 2)
+
+  expect_identical(df_append(df1, df2), data.frame(x = 1, y = 2))
+})
 
 test_that("can append at any integer position", {
   df1 <- data.frame(x = 1, y = 2)
   df2 <- data.frame(a = 1)
 
-  expect_named(append_df(df1, df2, 0L), c("a", "x", "y"))
-  expect_named(append_df(df1, df2, 1L), c("x", "a", "y"))
-  expect_named(append_df(df1, df2, 2L), c("x", "y", "a"))
+  expect_named(df_append(df1, df2, 0L), c("a", "x", "y"))
+  expect_named(df_append(df1, df2, 1L), c("x", "a", "y"))
+  expect_named(df_append(df1, df2, 2L), c("x", "y", "a"))
 })
 
 test_that("can append at any character position", {
   df1 <- data.frame(x = 1, y = 2)
   df2 <- data.frame(a = 1)
 
-  expect_named(append_df(df1, df2, "x"), c("x", "a", "y"))
-  expect_named(append_df(df1, df2, "y"), c("x", "y", "a"))
+  expect_named(df_append(df1, df2, "x"), c("x", "a", "y"))
+  expect_named(df_append(df1, df2, "y"), c("x", "y", "a"))
 })
 
 test_that("can replace at any character position ", {
   df1 <- data.frame(x = 1, y = 2, z = 3)
   df2 <- data.frame(a = 1)
 
-  expect_named(append_df(df1, df2, "x", remove = TRUE), c("a", "y", "z"))
-  expect_named(append_df(df1, df2, "y", remove = TRUE), c("x", "a", "z"))
-  expect_named(append_df(df1, df2, "z", remove = TRUE), c("x", "y", "a"))
+  expect_named(df_append(df1, df2, "x", remove = TRUE), c("a", "y", "z"))
+  expect_named(df_append(df1, df2, "y", remove = TRUE), c("x", "a", "z"))
+  expect_named(df_append(df1, df2, "z", remove = TRUE), c("x", "y", "a"))
 })
