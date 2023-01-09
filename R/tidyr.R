abbad912e88b135c57ac83a85ed574d3e5bb7bce
DavisVaughan <davis@rstudio.com>
2023-01-09 15:17:03 -0500

    Use `imap()` from purrr

--- R/tidyr.R
+++ R/tidyr.R
@@ -5,7 +5,7 @@
 #' @importFrom purrr accumulate discard every keep map map2 map2_chr
 #'   map2_dbl map2_df map2_int map2_lgl map_at map_chr map_dbl map_df
 #'   map_if map_int map_lgl pmap pmap_chr pmap_dbl pmap_df pmap_int
-#'   pmap_lgl reduce some transpose
+#'   pmap_lgl imap reduce some transpose
 #' @importFrom dplyr tbl_vars
 #' @importFrom utils type.convert
 #' @importFrom lifecycle deprecated
